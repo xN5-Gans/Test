968e5b4c114c93dbbcc2d2b19c56e8ae8212d806
xN5-Gans <77638633+xN5-Gans@users.noreply.github.com>
2021-02-14 18:02:23 +0700

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,31 @@
+
+ 
+import os
+import time
+
+os.system('clear')
+#xN5
+def U():
+#Xploiter
+#Recode Izin Bgst pyzluclufer@gmail.com
+	print("┌ｘＮ５ Ｘｐｌｏｉｔｅｒ╼")
+	time.sleep(2)
+	print("")
+	print("\33[31;1mINDONESIA CYBER NIGHTMARE")
+	time.sleep(2)
+	print("")
+	print('\33[33;1mNOTE┌Jika Eror Berarti Terlalu Banyak Web╼')
+	print("")
+	time.sleep(2)
+	print('\33[31;1mSorry Jika Masih Sering Ngebug')
+	print("")
+	time.sleep(1)
+	print('\33[32;1mHAPPY HACKING...')
+	print("")
+	time.sleep(2)
+U()
+from googlesearch import search
+query = str(input("\33[0;36m┌DORKNYA~>"))
+os.system('clear')
+for URL in search(query):
+  print(URL)
